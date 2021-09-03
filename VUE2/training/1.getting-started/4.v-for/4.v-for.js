d202e4baa368f765081c691c64725dba69be814b
vjudge <gradonday@gmail.com>
2021-09-03 20:38:58 +0800

    add v-for js

--- VUE2/training/1.getting-started/4.v-for/4.v-for.js
+++ VUE2/training/1.getting-started/4.v-for/4.v-for.js
@@ -5,6 +5,12 @@ var app = new Vue({
       { text: '学习 JavaScript' },
       { text: '学习 Vue' },
       { text: '整个牛项目' }
-    ]
+    ],
+    objects: {
+      JavaScript: '一直在学习',
+      Java: '打算学习',
+      Python: '学完了，实战中',
+      Go: '正在学习'
+    }
   }
 })
