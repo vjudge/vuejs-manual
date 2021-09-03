b4f9032492e69ecf3c8f46338d77090ae96ea08f
vjudge <gradonday@gmail.com>
2021-09-03 20:39:59 +0800

    update v-on js

--- VUE2/training/1.getting-started/6.v-model/6.v-model.js
+++ VUE2/training/1.getting-started/6.v-model/6.v-model.js
@@ -1,6 +1,18 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+    message: 'Hello Vue!',
+    areaMsg: '',
+    checkedNames: [],
+    picked: '',
+    selected: '',
+    multipleSelected: [],
+    vforselected: 'A',
+    options: [
+      { text: 'One', value: 'A' },
+      { text: 'Two', value: 'B' },
+      { text: 'Three', value: 'C' }
+    ],
+    toggle: 'no'
   }
 })
