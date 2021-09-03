6f866ec63377c33ac2b794323d59cd8cddeda0a4
vjudge <gradonday@gmail.com>
2021-09-03 20:39:30 +0800

    update v-on js

--- VUE2/training/1.getting-started/5.v-on/5.v-on.js
+++ VUE2/training/1.getting-started/5.v-on/5.v-on.js
@@ -1,11 +1,24 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue.js!'
+    message: 'Hello Vue.js!',
+    counter: 0,
+    name: 'clicked'
   },
   methods: {
     reverseMessage: function () {
       this.message = this.message.split('').reverse().join('')
+    },
+    greet: function (event) {
+      // `this` 在方法里指向当前 Vue 实例
+      alert(this.name)
+      // `event` 是原生 DOM 事件
+      if (event) {
+        alert(event.target.tagName)
+      }
+    },
+    say: function (msg) {
+      alert(msg)
     }
   }
 })
