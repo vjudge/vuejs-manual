ca2744b95eb78cf74bd399b059efae9c27c5a240
vjudge <gradonday@gmail.com>
2021-09-06 22:57:47 +0800

    update component

--- VUE2/training/1.getting-started/7.component/7.component.js
+++ VUE2/training/1.getting-started/7.component/7.component.js
@@ -2,6 +2,15 @@ Vue.component('todo-item', {
   template: '<li>这是个待办项</li>'
 })
 
+Vue.component('button-counter', {
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+  template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+})
+
 var app = new Vue({
   el: '#app'
 })
